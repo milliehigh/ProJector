6bcae4d635b12d6bdd9c2755ea01227571f5e25a
z5360024 <z5360024@unsw.edu.au>
2024-10-05 01:19:25 +1000

    adding more pages

---
+++ frontend/src/pages/AllProjects.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AllProjects() {
+    return (
+      <div>
+        <h1>Welcome to the Home Page</h1>
+        <p>Search Bar here.</p>
+      </div>
+    );
+}
+
+export default AllProjects;
