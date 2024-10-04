7dd1ef1fe5ed48fbb49be0bdaf6228a5cebc2db9
z5360024 <z5360024@unsw.edu.au>
2024-10-05 01:23:03 +1000

    created navigation bar

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import { AppBar, Box, Button, Typography, Toolbar, CssBaseline } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+
+function Header() {
+  console.log('header')
+  const navigate = useNavigate();
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+        <CssBaseline />
+      <AppBar position="sticky" sx={{backgroundColor: "#344649"}}>
+        <Toolbar sx={{justifyContent: "center"}}>
+          <Typography
+            variant="h5"
+            onClick={() => { navigate('/') }}
+          >
+            ProjectOr
+          </Typography>
+          {/* { localStorage.getItem('token') ? ( */} 
+          <>
+          <Box>
+            <Button name="dashbaord" color="inherit" onClick={() => { navigate('dashboard') }} sx={{ textTransform: 'none' }}>Dashboard</Button>
+            <Button name="allProjects" color="inherit" onClick={() => { navigate('allprojects') }} sx={{ textTransform: 'none' }}>Browse Projects</Button>
+            <Button name="aboutus" color="inherit" onClick={() => { navigate('aboutus') }} sx={{ textTransform: 'none' }}>About Us</Button>
+
+          </Box>
+           
+            {/* <Button name="logoutBtn" color="inherit" onClick={logout}>Logout</Button> */}
+          {/* </>) : ( */}
+          {/* <> */}
+          <Box>
+            <><Button name="registerBtn" color="green" onClick={() => { navigate('register') } }>Register</Button></>
+            <Button name="loginBtn" sx={{backgroundColor: "#F29465"}} onClick={() => { navigate('login') } }>Login</Button>
+          </Box>
+            </>
+              {/* )} */}
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
+
+export default Header;
