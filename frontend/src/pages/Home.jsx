c530e1414f95d9924bab74c0bd416feabb808cd6
Andrew <z5359366@ad.unsw.edu.au>
2024-10-05 15:01:40 +1000

    Created the home page of the website

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React from "react";
+import styles from "../styles/Home.module.css";
 
 function Home() {
-    return (
-      <div>
-        <h1>Welcome to the Home Page</h1>
-        <p>Search Bar here.</p>
+  return (
+    <div className={styles.titleContainer}>
+      <h1 className={styles.title}>Find Your Dream Project Today!</h1>
+      <div class={styles.searchBar}>
+        <input 
+          type="text" 
+          className={styles.search}
+          placeholder="Search for a Project Title or Company" 
+        />
+        <button className={styles.searchButton}>
+          🔍
+        </button>
       </div>
-    );
+    </div>
+  );
 }
 
 export default Home;
