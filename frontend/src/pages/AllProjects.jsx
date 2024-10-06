09122910d2fd2aaaa8b1baa7e007a5e621139960
Millie <millie.hai1827@gmail.com>
2024-10-06 18:48:23 +1100

    created file layout

--- frontend/src/pages/AllProjects.jsx
+++ frontend/src/pages/AllProjects.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
+import Box from '@mui/material/Box';
+import Collapse from '@mui/material/Collapse';
 
 function AllProjects() {
+    const [checked, setChecked] = React.useState(false);
+
+    const handleChange = () => {
+        setChecked((prev) => !prev);
+    };
     return (
-      <div>
-        <h1>Welcome to the Home Page</h1>
-        <p>Search Bar here.</p>
-      </div>
+      <Box>
+        <Box sx={{ width: '45%', height: '100%', backgroundColor: 'grey' }}>
+          <Collapse orientation="horizontal" in={checked} sx={{ height: '100%'}}>
+          <div>
+          <h1>Welcome to the Home Page</h1>
+          <p>Search Bar here.</p>
+        </div>
+          </Collapse>
+        </Box>
+        {/* <div>
+          <h1>Welcome to the Home Page</h1>
+          <p>Search Bar here.</p>
+        </div> */}
+      </Box>
+      
     );
 }
 
