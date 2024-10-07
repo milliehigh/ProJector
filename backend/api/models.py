d2bb4157da5514eb78bed01e6591004cdef55198
Blair <z5351042@ad.unsw.edu.au>
2024-10-07 18:21:56 +1100

    This reverts commit d1772632bcad2399148699d9f3c64916fc42570d.
    crazy push to main

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.core.validators import EmailValidator
-from django.core.validators import URLValidator
 
 class Note(models.Model):
     title = models.CharField(max_length=100)
@@ -11,27 +9,3 @@ class Note(models.Model):
 
     def __str__(self):
         return self.title
-    
-class Company(models.Model):
-    id = models.CharField(unique=True)
-    password = models.CharField() #options
-    name = models.CharField()
-    email = models.EmailField(unique=True, validators=[EmailValidator])
-    phonenumber = models.CharField()
-    website = models.URLField(validators=[URLValidator])
-    logo = models.ImageField(upload_to ='uploads/') 
-    description = models.TextField()
-
-class Professional(models.Model):
-    id = models.CharField(unique=True)
-    password = models.CharField() #options
-    lastname = models.CharField()
-    firstname = models.CharField()
-    profilepic = models.ImageField(upload_to ='uploads/') 
-    email = models.EmailField(unique=True, validators=[EmailValidator])
-    website = models.URLField(validators=[URLValidator])
-    phonenumber = models.CharField()
-    description = models.TextField()
-    qualification = models.URLField(validators=[URLValidator])
-    education = models #options
-    skills = models #options
