bb8de3670542fa4de74ee2e38a2b8fe7dab4f7f2
Ce Min Pangastur <cm.pang25@gmail.com>
2024-10-08 00:16:44 +1100

    added register page

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,7 +1,78 @@
-import Form from "../components/Form"
+import { useState } from "react"
+import CompanyRegisterForm from "../components/CompanyRegisterForm";
+import { Box, Button } from "@mui/material";
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import ProfessionalRegisterForm from "../components/ProfessionalRegisterForm";
+import ProjectCandidateList from "./ProjectCandidateList";
 
 function Register() {
-    return <Form route="/api/user/register/" method="register" />
+
+    const [showForm, setShowForm] = useState(false);
+    const [isCompany, setIsCompany] = useState(false);
+
+    const handleClick = (type) => {
+        setShowForm(true)
+        setIsCompany(type == "company" ? true : false)
+    }
+
+    const handleClickBack = () => {
+        setShowForm(false)
+    }
+
+    return (
+        <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            flexDirection="column"
+        >
+            {showForm ? 
+                (
+                    <>
+                        {isCompany ?
+                            <CompanyRegisterForm /> :
+                            <ProfessionalRegisterForm />
+                        }
+                        <Button 
+                            variant="outlined"
+                            startIcon={<ArrowBackIcon />}
+                            onClick={handleClickBack}
+                        >
+                            Back
+                        </Button>
+                    </>
+                    
+                ) : (
+                    <>
+                        <h1>Are you a company or a professional?</h1>
+                        <Box
+                            display="flex"
+                            justifyContent="space-between"
+                            alignItems="center"
+                            width="50vw"
+                        >
+                            <Button 
+                                onClick={() => handleClick("company")}
+                                variant="contained"
+                                sx={{ height: '30vh', width: "20vw" }}
+                            >
+                                Company
+                            </Button>
+                            <Button 
+                                onClick={() => handleClick("professional")}
+                                variant="contained"
+                                sx={{ height: '30vh', width: "20vw" }}
+                            >
+                                Professional
+                            </Button>
+                        </Box>
+                        <ProjectCandidateList />
+                    </>
+                )
+            }
+        </Box>
+
+    )
 }
 
 export default Register
