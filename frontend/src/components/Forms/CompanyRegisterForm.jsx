154c37ded707b3876f117d68a4c7d7cef2deff6b
Ce Min Pangastur <cm.pang25@gmail.com>
2024-10-08 22:39:14 +1100

    added api

--- frontend/src/components/Forms/CompanyRegisterForm.jsx
+++ frontend/src/components/Forms/CompanyRegisterForm.jsx
@@ -3,107 +3,138 @@ import Form from "./Form"
 import { Button, styled, TextField } from "@mui/material";
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 import VisuallyHiddenInput from "../VisuallyHiddenInput";
-
+import { ACCESS_TOKEN, REFRESH_TOKEN } from "../../constants";
+import api from "../../api";
+import { useNavigate } from "react-router-dom";
 
 function CompanyRegisterForm() {
-    const [loading, setLoading] = useState(false);
-    const [formData, setFormData] = useState({
-        name: "",
-        email: "",
-        phoneNumber: "",
-        website: "",
-        logo: "",
+  const [loading, setLoading] = useState(false);
+  const [formData, setFormData] = useState({
+    companyName: "",
+    companyEmail: "",
+    companyPassword: "",
+    companyPhoneNumber: "",
+    companyWebsite: "",
+    companyLogo: "",
+    companyDescription: ""
+  });
+
+  const navigate = useNavigate();
+
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
     });
+  }
 
-    const onChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({
-            ...formData,
-            [name]: value
-        });
+  // Submit registration
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    try {
+      // Handle API here
+      console.log(formData)
+      const username = formData.companyEmail
+      const password = formData.companyPassword
+      const res = await api.post("/api/user/register/", { username, password });
+      localStorage.setItem(ACCESS_TOKEN, res.data.access);
+      localStorage.setItem(REFRESH_TOKEN, res.data.refresh);
+      navigate("/prodashbaord")
+    } catch (error) {
+      alert(error)
+    } finally {
+      setLoading(false)
     }
-
-    //// turn into async
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        setLoading(true);
-
-        try {
-            // Handle API here /////////////
-            console.log(formData)
-
-        } catch (error) {
-            alert(error)
-        } finally {
-            setLoading(false)
-        }
-    }
+  }
 
 
-    return (
-        <Form 
-            route="/api/user/companyRegister/" 
-            formName="Company Register"
-            buttonName="Register"
-            handleSubmit={handleSubmit}
-        >
-            <TextField
-                variant="filled"
-                margin="normal"
-                className="form-input"
-                label="Company Name"
-                name="name"
-                value={formData.name}
-                onChange={onChange}
-            />
-            <TextField
-                variant="filled"
-                margin="normal"
-                className="form-input"
-                type="text"
-                label="Company Email"
-                name="email"
-                value={formData.email}
-                onChange={onChange}
-            />
-            <TextField
-                variant="filled"
-                margin="normal"
-                className="form-input"
-                type="text"
-                label="Company Phone Number"
-                name="phoneNumber"
-                value={formData.phoneNumber}
-                onChange={onChange}
-            />
-            <TextField
-                variant="filled"
-                margin="normal"
-                className="form-input"
-                type="text"
-                label="Company Website"
-                name="website"
-                value={formData.website}
-                onChange={onChange}
-            />
-            <Button
-                sx={{ margin: '16px 0' }}
-                className="form-input"
-                component="label"
-                variant="contained"
-                startIcon={<CloudUploadIcon />}
-            >
-                Upload files
-                <VisuallyHiddenInput
-                    type="file"
-                    accept="image/*"
-                    name="logo"
-                    value={formData.logo}
-                    onChange={onChange}
-                />
-            </Button>
-        </Form>
-    )
+  return (
+    <Form 
+      route="/api/user/companyRegister/" 
+      formName="Company Register"
+      buttonName="Register"
+      handleSubmit={handleSubmit}
+    >
+      <TextField
+        variant="filled"
+        margin="normal"
+        className="form-input"
+        label="Company Name"
+        name="companyName"
+        value={formData.companyName}
+        onChange={onChange}
+      />
+      <TextField
+        variant="filled"
+        margin="normal"
+        className="form-input"
+        type="text"
+        label="Company Email"
+        name="companyEmail"
+        value={formData.companyEmail}
+        onChange={onChange}
+      />
+      <TextField
+        variant="filled"
+        margin="normal"
+        className="form-input"
+        type="text"
+        label="Company Password"
+        name="companyPassword"
+        value={formData.companyPassword}
+        onChange={onChange}
+      />
+      <TextField
+        variant="filled"
+        margin="normal"
+        className="form-input"
+        type="text"
+        label="Company Phone Number"
+        name="companyPhoneNumber"
+        value={formData.companyPhoneNumber}
+        onChange={onChange}
+      />
+      <TextField
+        variant="filled"
+        margin="normal"
+        className="form-input"
+        type="text"
+        label="Company Website"
+        name="companyWebsite"
+        value={formData.companyWebsite}
+        onChange={onChange}
+      />
+      <TextField
+        variant="filled"
+        margin="normal"
+        className="form-input"
+        type="text"
+        label="Company Description"
+        name="companyDescription"
+        value={formData.companyDescription}
+        onChange={onChange}
+      />
+      <Button
+        sx={{ margin: '16px 0' }}
+        className="form-input"
+        component="label"
+        variant="contained"
+        startIcon={<CloudUploadIcon />}
+      >
+        Upload files
+        <VisuallyHiddenInput
+            type="file"
+            accept="image/*"
+            name="companyLogo"
+            value={formData.companyLogo}
+            onChange={onChange}
+        />
+      </Button>
+    </Form>
+  )
 }
 
 export default CompanyRegisterForm
--- frontend/src/components/Forms/ProfessionalRegisterForm.jsx
+++ frontend/src/components/Forms/ProfessionalRegisterForm.jsx
@@ -3,142 +3,172 @@ import Form from "./Form"
 import { Button, TextField } from "@mui/material";
 import VisuallyHiddenInput from "../VisuallyHiddenInput";
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import { ACCESS_TOKEN, REFRESH_TOKEN } from "../../constants";
+import api from "../../api";
+import { useNavigate } from "react-router-dom";
 
 
 function ProfessionalRegisterForm() {
-    const [loading, setLoading] = useState(false);
-    const [formData, setFormData] = useState({
-        fullName: "",
-        email: "",
-        phoneNumber: "",
-        website: "",
-        biography: "",
-        education: "",
-        qualifications: "",
-        skills: "",
-        photo: ""
+  const [loading, setLoading] = useState(false);
+  const [formData, setFormData] = useState({
+    professionalFullName: "",
+    professionalEmail: "",
+    professionalPassword: "",
+    professionalWebsite: "",
+    professionalPhoneNumber: "",
+    professionalDescription: "",
+    professionalQualifications: "",
+    professionalEducation: "",
+    professionalSkills: "",
+    professionalPhoto: ""
+  });
+
+  const navigate = useNavigate();
+
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
     });
+  }
 
-    const onChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({
-            ...formData,
-            [name]: value
-        });
+  // Submit registration
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    try {
+      // Handle API here
+      console.log(formData)
+      const username = formData.professionalEmail
+      const password = formData.professionalPassword
+      const res = await api.post("/api/user/register/", { username, password });
+      localStorage.setItem(ACCESS_TOKEN, res.data.access);
+      localStorage.setItem(REFRESH_TOKEN, res.data.refresh);
+      navigate("/prodashbaord")
+    } catch (error) {
+      alert(error)
+    } finally {
+      setLoading(false)
     }
+  }
 
-    //// turn into async
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        setLoading(true);
-
-        try {
-            // Handle API here /////////////
-            console.log(formData)
-
-        } catch (error) {
-            alert(error)
-        } finally {
-            setLoading(false)
-        }
-    }
-
-    return (
-        <Form
-            route="/api/user/professionalRegister/" 
-            formName="Professional Register"
-            buttonName="Register"
-            handleSubmit={handleSubmit}
-        >
-            <TextField
-                variant="filled"
-                margin="normal"
-                className="form-input"
-                type="text"
-                label="Full name"
-                name="fullName"
-                value={formData.fullName}
-                onChange={onChange}
-            />
-            <TextField
-                variant="filled"
-                margin="normal"
-                className="form-input"
-                type="text"
-                label="Email"
-                name="email"
-                value={formData.email}
-                onChange={onChange}
-            />
-            <TextField
-                variant="filled"
-                margin="normal"
-                className="form-input"
-                type="text"
-                label="Phone Number"
-                name="phoneNumber"
-                value={formData.phoneNumber}
-                onChange={onChange}
-            />
-            <TextField
-                variant="filled"
-                margin="normal"
-                className="form-input"
-                type="text"
-                label="Biography"
-                name="biography"
-                value={formData.biography}
-                onChange={onChange}
-            />
-            <TextField
-                variant="filled"
-                margin="normal"
-                className="form-input"
-                type="text"
-                label="Education"
-                name="education"
-                value={formData.education}
-                onChange={onChange}
-            />
-            <TextField
-                variant="filled"
-                margin="normal"
-                className="form-input"
-                type="text"
-                label="Qualifications"
-                name="qualifications"
-                value={formData.qualifications}
-                onChange={onChange}
-            />
-            {/*  */}
-            <TextField
-                variant="filled"
-                margin="normal"
-                className="form-input"
-                type="text"
-                label="Skills"
-                name="skills"
-                value={formData.skills}
-                onChange={onChange}
-            />
-            <Button
-                sx={{ margin: '16px 0' }}
-                className="form-input"
-                component="label"
-                variant="contained"
-                startIcon={<CloudUploadIcon />}
-            >
-                Upload files
-                <VisuallyHiddenInput
-                    type="file"
-                    accept="image/*"
-                    name="photo"
-                    value={formData.photo}
-                    onChange={onChange}
-                />
-            </Button>
-        </Form>
-    )
+  return (
+    <Form
+      route="/api/user/professionalRegister/" 
+      formName="Professional Register"
+      buttonName="Register"
+      handleSubmit={handleSubmit}
+    >
+      <TextField
+        variant="filled"
+        margin="normal"
+        className="form-input"
+        type="text"
+        label="Full name"
+        name="professionalFullName"
+        value={formData.professionalFullName}
+        onChange={onChange}
+      />
+      <TextField
+        variant="filled"
+        margin="normal"
+        className="form-input"
+        type="text"
+        label="Email"
+        name="professionalEmail"
+        value={formData.professionalEmail}
+        onChange={onChange}
+      />
+      <TextField
+        variant="filled"
+        margin="normal"
+        className="form-input"
+        type="text"
+        label="Password"
+        name="professionalPassword"
+        value={formData.professionalPassword}
+        onChange={onChange}
+      />
+      <TextField
+        variant="filled"
+        margin="normal"
+        className="form-input"
+        type="text"
+        label="Website"
+        name="professionalWebsite"
+        value={formData.professionalWebsite}
+        onChange={onChange}
+      />
+      <TextField
+        variant="filled"
+        margin="normal"
+        className="form-input"
+        type="text"
+        label="Phone Number"
+        name="professionalPhoneNumber"
+        value={formData.professionalPhoneNumber}
+        onChange={onChange}
+      />
+      <TextField
+        variant="filled"
+        margin="normal"
+        className="form-input"
+        type="text"
+        label="Description"
+        name="professionalDescription"
+        value={formData.professionalDescription}
+        onChange={onChange}
+      />
+      <TextField
+        variant="filled"
+        margin="normal"
+        className="form-input"
+        type="text"
+        label="Qualifications"
+        name="professionalQualifications"
+        value={formData.professionalQualifications}
+        onChange={onChange}
+      />
+      <TextField
+        variant="filled"
+        margin="normal"
+        className="form-input"
+        type="text"
+        label="Education"
+        name="professionalEducation"
+        value={formData.professionalEducation}
+        onChange={onChange}
+      />
+      <TextField
+        variant="filled"
+        margin="normal"
+        className="form-input"
+        type="text"
+        label="Skills"
+        name="professionalSkills"
+        value={formData.professionalSkills}
+        onChange={onChange}
+      />
+      <Button
+        sx={{ margin: '16px 0' }}
+        className="form-input"
+        component="label"
+        variant="contained"
+        startIcon={<CloudUploadIcon />}
+      >
+        Upload files
+        <VisuallyHiddenInput
+          type="file"
+          accept="image/*"
+          name="professionalPhoto"
+          value={formData.professionalPhoto}
+          onChange={onChange}
+        />
+      </Button>
+    </Form>
+  )
 }
 
 export default ProfessionalRegisterForm
