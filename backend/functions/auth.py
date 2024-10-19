1148b0a1964fb31155ff5737e8258bc3f7d5ccb3
Ce Min Pangastur <cm.pang25@gmail.com>
2024-10-19 14:06:17 +1100

    added skeleton

--- backend/functions/auth.py
+++ backend/functions/auth.py
@@ -1,10 +1,52 @@
-from flask import Flask
+from flask import Flask, request
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import create_access_token
+# from src.error import AccessErrror
 
 app = Flask(__name__)
 
 @app.route('/auth/register/company', methods=['POST'])
+# def authRegisterCompany(companyName: str, companyEmail: str, companyPassword: str, 
+#                         companyPhoneNumber: str, companyWebsite: str, 
+#                         companyDescription: str, companyLogo: str):
 def authRegisterCompany():
-    return
+    data = request.get_json()
+
+    # Error checking ###########
+    
+
+    # Get data (password, userId)
+    password = generate_password_hash(data.get("password"))
+    user_id = 1 ### change this #################
+
+
+    # Store in database ######
+
+
+
+    ''' javascript frontend
+    import jwt_decode from "jwt-decode";
+
+    // Assuming you have the JWT token stored in localStorage or retrieved from your API
+    const token = localStorage.getItem('accessToken');
+
+    // Decode the token to access the additional claims
+    const decodedToken = jwt_decode(token);
+
+    console.log(decodedToken);
+    '''
+
+    # Create data for token
+    token_data = {
+        "userId": user_id,
+        "userType": "company"
+    }
+
+    # Create token
+    access_token = create_access_token(identity=data.get("email"), additional_claims=token_data)
+
+    # Return the token
+    return { "token": access_token }
 
 @app.route('/auth/register/professional', methods=['POST'])
 def authRegisterProfessional():
@@ -12,6 +54,19 @@ def authRegisterProfessional():
 
 @app.route('/auth/login', methods=['POST'])
 def login():
+    data = request.get_json()
+
+    # Check if the user exists (based on email)
+    email = data.get("email")
+
+    # Get password
+    password = "123"
+
+    # Check if password is correct
+    ##### might need to do the hashing thing
+    # if password != data.get("password"):
+    #     return AccessError
+
     return
 
 @app.route('/auth/logout', methods=['POST'])
