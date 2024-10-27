f17277809b6c34ac4ede018162f8b74d8a9af780
Millie <millie.hai1827@gmail.com>
2024-10-27 19:35:57 +1100

    page contianer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 
-// import NavBar from './components/NavBar';
 import Header from './components/Header';
 
 
