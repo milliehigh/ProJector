05480a8fc72c74192d24edc2616d0a647cea716e
Jerry Li <jerryli0320@gmail.com>
2024-10-30 17:47:00 +1100

    deleted console.log

--- frontend/src/pages/AllProjects.jsx
+++ frontend/src/pages/AllProjects.jsx
@@ -131,7 +131,6 @@ export default function AllProjects() {
   React.useEffect(() => {
     apiGet("/project/listall",)
     .then((data) => {
-        console.log(data);
         if (!data.error) {
           setAllProjects(data);
           // console.log("project details:", allProjects)
