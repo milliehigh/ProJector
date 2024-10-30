976f126db25d11005ac24f38c0135f885b7025c8
Ce Min Pangastur <cm.pang25@gmail.com>
2024-10-30 19:11:16 +1100

    set up for adding checks if they are the company themself

--- frontend/src/pages/ProjectApplicantList.jsx
+++ frontend/src/pages/ProjectApplicantList.jsx
@@ -3,20 +3,49 @@ import ProjectCard from "../components/Professional/Dashboard/ProjectCard";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
 import { apiGet } from "../api";
+import decodeJWT from "../decodeJWT";
 
 function ProjectApplicantList() {
   const params = useParams();
   const [projectList, setProjectList] = useState([]);
+  // Set professional as default as it is the minimal amount of perms
+  const [userType, setUserType] = useState("professional");
+  const [currUserId, setCurrUserId] = useState(0);
 
-  // insert api call here (turn into async)
+  // Check if they are the respective company
   useEffect(() => {
-    // API call....
-    console.log(`projectId = ${params.projectId.replace(":","")}`);
+    // Check if they are a professional
+    const token = localStorage.getItem("token");
+    const tokenData = decodeJWT(token);
+    setUserType(tokenData.userType);
 
-    apiGet("/project/applicant/list", `projectId=${params.projectId.replace(":","")}`)
+    if (tokenData.userType == "professional") {
+      return;
+    }
+
+    console.log(`projectId = ${params.projectId.replace(":","")}`)
+    apiGet("/project/details", `projectId=${params.projectId.replace(":","")}`)
       .then((data) => {
         if (!data.error) {
           console.log(`data = ${data}`)
+          console.log(`data JSON = ${JSON.stringify(data[0], null, 2)}`);
+          // setCurrUserId(data.pCompanyId)
+        } else {
+          throw new Error("Get Project Candidate List Failed");
+        }
+      })
+      .catch(() => {
+        alert("error when getting project details")
+      });
+
+  }, [])
+
+  // insert api call here (turn into async)
+  useEffect(() => {
+    // API call
+    apiGet("/project/applicant/list", `projectId=${params.projectId.replace(":","")}`)
+      .then((data) => {
+        if (!data.error) {
           setProjectList(data)
           console.log(JSON.stringify(data[0], null, 2));
         } else {
@@ -27,46 +56,46 @@ function ProjectApplicantList() {
         alert("error")
       })
 
-    // try {
-    //   const res = await axios.get(`/projectCandidateList${projectId}`, "GET")
-    //   console.log('Data:', res.data);
-    //   setProjectList(res.) // data structure of the response
-    // } catch (error) {
-    //   alert(error)
-    // }
-
   }, [])
 
   return (
-    <Box
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      flexDirection="column"
-    >
-      <Typography
-        variant="h4"
-        gutterBottom
-      >
-        Project Applicant List
-      </Typography>
+    
+    userType === "company" ?
       <Box
         display="flex"
         justifyContent="center"
         alignItems="center"
         flexDirection="column"
-        width="80vw"
       >
-        {projectList.map((applicant, index) => (
-          <ProjectCard
-            key={index}
-            projectName={applicant.professionalFullName}
-            projectDescription={applicant.professionalSkills.join(', ')}
-            // projectDescription={applicant.professionalEmail}
-          />
-        ))}
+        <Typography
+          variant="h4"
+          gutterBottom
+        >
+          Project Applicant List
+        </Typography>
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          flexDirection="column"
+          width="80vw"
+        >
+          {projectList.map((applicant, index) => (
+            <ProjectCard
+              key={index}
+              projectName={applicant.professionalFullName}
+              projectDescription={applicant.professionalSkills.join(', ')}
+              // projectDescription={applicant.professionalEmail}
+            />
+          ))}
+        </Box>
       </Box>
-    </Box>
+      : 
+      <Box>
+        <Typography variant="h1">
+          No access allowed
+        </Typography>
+      </Box>
   );
 }
 
