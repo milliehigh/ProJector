4c91308ec2fe8636d609cead25ad8c93468eb2ef
Millie <millie.hai1827@gmail.com>
2024-11-04 02:36:03 +1100

    review page done

---
+++ frontend/src/pages/ReviewPage.jsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import { Link, useNavigate, Navigate , useParams } from 'react-router-dom';
+import { apiGet } from '../api';
+import RateProfessional from './RateProfessional';
+import RateProject from './RateProject';
+import decodeJWT from '../decodeJWT';
+import { PageContainer } from '@toolpad/core/PageContainer';
+
+
+const ReviewPage = () => {
+    const params = useParams();
+    const [userType, setUserType] = React.useState('');
+    // const [userId, setUserId] = React.useState(params.userId.replace(":",""));
+
+    React.useEffect(() => {
+        const getToken = localStorage.getItem("token");
+        if (getToken != null) {
+            const tokenData = decodeJWT(getToken);
+            // setOwnUserId(tokenData.userId);
+            setUserType(tokenData.userType);
+            console.log(tokenData.userType)
+        }
+    }, []);
+
+    return (
+        <PageContainer className="container" maxWidth={false} sx={{width:"100%", "@media (min-width: 0px)": { paddingRight: "25px", paddingLeft: "25px" }, margin: "0px"}}>
+        <>
+        { userType ? (userType === "company" ?  <RateProfessional projectId={params}></RateProfessional>  :  <RateProject projectId={params} ></RateProject>) 
+        : (<></>) }
+        </>
+        </PageContainer>
+    );
+}
+
+export default ReviewPage;
