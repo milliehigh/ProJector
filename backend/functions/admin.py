74a4891049ac44e35a56a7d8f8d02d5e3572edd2
Ce Min Pangastur <cm.pang25@gmail.com>
2024-11-07 23:41:18 +1100

    added admin functions

---
+++ backend/functions/admin.py
@@ -0,0 +1,48 @@
+from flask import Flask, jsonify, request
+from database.models import Company, Professional, Admin
+
+app = Flask(__name__)
+
+@app.route('/admin/allCompanies', methods=['GET'])
+def allCompanies():
+    companies = Company.query.all()
+    companyList = [
+        {
+            "id": company.companyId,
+            "companyName": company.companyName,
+            "companyEmail": company.companyEmail,
+            "companyPhoneNumber": company.companyPhoneNumber,
+            "companyWebsite": company.companyWebsite
+        }
+    for company in companies
+    ]
+
+    return jsonify(companyList), 200
+
+@app.route('/admin/allProfessionals', methods=['GET'])
+def allProfessionals():
+    professionals = Professional.query.all()
+    professionalList = [
+        {
+            "id": professional.professionalId,
+            "professionalFullName": professional.professionalFullName,
+            "professionalEmail": professional.professionalEmail,
+            "professionalPhoneNumber": professional.professionalPhoneNumber,
+        }
+    for professional in professionals
+    ]
+
+    return jsonify(professionalList), 200
+
+@app.route('/admin/allAdmins', methods=['GET'])
+def allAdmins():
+    admins = Admin.query.all()
+    adminList = [
+        {
+            "id": admin.adminId,
+            "adminEmail": admin.adminEmail,
+        }
+    for admin in admins
+    ]
+
+    return jsonify(adminList), 200
