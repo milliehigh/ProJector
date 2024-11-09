558d2c4e1374af6d2c61734b09a2060fbc2e33c0
Ce Min Pangastur <cm.pang25@gmail.com>
2024-11-09 19:55:50 +1100

    added dashboard

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import AdminDashboard from "./AdminDashboard";
+import ProfessionalDashboard from "./ProfessionalDashboard";
+import CompanyDashboard from "./CompanyDashboard";
+import decodeJWT from "../decodeJWT";
+import NoAccessPage from "./NoAccessPage";
+
+export default function Dashboard() {
+  const [userType, setUserType] = useState(null);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      const decoded = decodeJWT(token);
+      if (decoded && decoded.userType) {
+        setUserType(decoded.userType);
+      } else {
+        setUserType("none");
+      }
+    } else {
+      setUserType("none");
+    }
+  }, []);
+
+  if (userType === null) {
+    // Show a loading state or some placeholder until userType is determined
+    return <p>Loading...</p>;
+  }
+
+  return (
+    userType === "admin" ? <AdminDashboard />
+    : (userType === "professional" ? <ProfessionalDashboard />
+      : (userType === "company" ? <CompanyDashboard />
+        : <NoAccessPage />
+      )
+    )
+  );
+}
