d54a2edaaeec1d7c137bb31f195ea71979a45934
Ce Min Pangastur <cm.pang25@gmail.com>
2024-11-11 23:43:58 +1100

    trialing new datatable component + project applicant list

---
+++ frontend/src/components/DataTable.jsx
@@ -0,0 +1,41 @@
+import { useNavigate } from "react-router-dom";
+import { DataGrid } from "@mui/x-data-grid";
+import Paper from "@mui/material/Paper";
+
+const paginationModel = { page: 0, pageSize: 5 };
+
+function DataTable({ rows, columns, onSelectionChange }) {
+  const navigate = useNavigate();
+
+  // Link to the respective profile page
+  const handleOnRowClick = (rowData) => {
+    // If it is part of the admin table do nothing
+    for (const column of rowData.columns) {
+      if ("field" in column) {
+        if (column["field"] === "adminEmail") {
+          return
+        }
+      }
+    }
+    navigate(`/profile/${rowData.id}`);
+  }
+
+  return (
+    // <Paper sx={{ height: "auto", minHeight: 200, width: "100%" }}>
+    <Paper sx={{ height: 400, width: "100%" }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        initialState={{ pagination: { paginationModel } }}
+        pageSizeOptions={[5, 10]}
+        checkboxSelection
+        disableRowSelectionOnClick 
+        onRowSelectionModelChange={(newSelection) => onSelectionChange(newSelection)}
+        onRowClick={handleOnRowClick}
+        sx={{ border: 0 }}
+      />
+    </Paper>
+  );
+}
+
+export default DataTable;
