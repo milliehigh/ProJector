f9b67ce62412b9b538474ade15d4e3da4613a1c7
Millie <millie.hai1827@gmail.com>
2024-11-12 19:04:41 +1100

    testing page container

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,7 +3,8 @@ import styles from "../styles/Home.module.css";
 
 function Home() {
   return (
-    <div className={styles.titleContainer}>
+    <>
+    {/* <div className={styles.titleContainer}> */}
       <h1 className={styles.title}>Find Your Dream Project Today!</h1>
       <div class={styles.searchBar}>
         <input 
@@ -11,11 +12,12 @@ function Home() {
           className={styles.search}
           placeholder="Search for a Project Title or Company" 
         />
-        <button className={styles.searchButton}>
-          🔍
-        </button>
-      </div>
-    </div>
+         <button className={styles.searchButton}>
+           🔍
+         </button>
+       </div>
+     {/* </div> */}
+    </>
   );
 }
 
