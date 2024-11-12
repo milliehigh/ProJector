0dd3d39d42d837d824b6c03597aaa89599f3ff2f
Ce Min Pangastur <cm.pang25@gmail.com>
2024-11-12 12:27:41 +1100

    added professional team

--- frontend/src/components/ProjectProfessionalList.jsx
+++ frontend/src/components/ProjectProfessionalList.jsx
@@ -16,6 +16,7 @@ const professionalColumns = [
 
 function ProjectProfessionalList({ projectId }) {
   const [update, setUpdate] = useState(false);
+  const [applicantRows, setApplicantRows] = useState([]);
   const [professionalRows, setProfessionalRows] = useState([]);
   const [selectedProfessionalRowIds, setSelectedProfessionalRowIds] = useState([]);
 
@@ -55,13 +56,30 @@ function ProjectProfessionalList({ projectId }) {
       });
   }
 
-  // Get applicant list
+  // Get applicant list and team list
   useEffect(() => {
     apiGet("/project/applicant/list", `projectId=${projectId}`)
       .then((data) => {
         if (!data.error) {
           const newData = data
             .filter((professional) => professional.status === "Pending approval")
+            .map(({ professionalId, ...rest }) => ({
+              "id": professionalId,
+              ...rest
+            }));
+          setApplicantRows(newData);
+        } else {
+          throw new Error();
+        }
+      })
+      .catch((data) => {
+        alert(data.error)
+      })
+
+    apiGet("/project/professional/list", `projectId=${projectId}`)
+      .then((data) => {
+        if (!data.error) {
+          const newData = data
             .map(({ professionalId, ...rest }) => ({
               "id": professionalId,
               ...rest
@@ -74,35 +92,43 @@ function ProjectProfessionalList({ projectId }) {
       .catch((data) => {
         alert(data.error)
       })
-  }, [update])
+  }, [update]);
 
   return (
-    <Box display="flex" flexDirection="column" width="100%">
-      <Box display="flex" flexDirection="row" gap="10px">
-        <Button 
-          onClick={handleApproveApplicant} 
-          variant="contained" 
-          color="success"
-          disabled={selectedProfessionalRowIds.length === 0}
-          startIcon={<ThumbUp />} 
-        >
-          Approve
-        </Button>
-        <Button 
-          onClick={handleRejectApplicant} 
-          variant="contained" 
-          color="error"
-          disabled={selectedProfessionalRowIds.length === 0}
-          startIcon={<ThumbDown />} 
-        >
-          Reject
-        </Button>
+    <Box display="flex" flexDirection="column" gap="50px" >
+      <Box display="flex" flexDirection="column" width="100%">
+        <Box display="flex" flexDirection="row" gap="10px">
+          <Button 
+            onClick={handleApproveApplicant} 
+            variant="contained" 
+            color="success"
+            disabled={selectedProfessionalRowIds.length === 0}
+            startIcon={<ThumbUp />} 
+          >
+            Approve
+          </Button>
+          <Button 
+            onClick={handleRejectApplicant} 
+            variant="contained" 
+            color="error"
+            disabled={selectedProfessionalRowIds.length === 0}
+            startIcon={<ThumbDown />} 
+          >
+            Reject
+          </Button>
+        </Box>
+        <DataTable 
+          rows={applicantRows} 
+          columns={professionalColumns} 
+          onSelectionChange={setSelectedProfessionalRowIds} 
+          checkboxSelection={true}
+        />
       </Box>
+
       <DataTable 
         rows={professionalRows} 
         columns={professionalColumns} 
-        onSelectionChange={setSelectedProfessionalRowIds} 
-        checkboxSelection={true}
+        checkboxSelection={false}
       />
     </Box>
   );
