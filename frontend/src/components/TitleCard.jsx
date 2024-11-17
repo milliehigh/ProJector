3777f65eace599d8f871df8b94fc4e19d7c44dce
Andrew <z5359366@ad.unsw.edu.au>
2024-11-18 10:42:14 +1100

    edited styling for some components

--- frontend/src/components/TitleCard.jsx
+++ frontend/src/components/TitleCard.jsx
@@ -1,26 +1,34 @@
 import * as React from 'react';
 import styles from"../styles/Professional/Dashboard.module.css";
 import { useNavigate } from 'react-router-dom';
-import StarIcon from '@mui/icons-material/Star';
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import AccessTimeFilledIcon from '@mui/icons-material/AccessTimeFilled';
 import WorkspacePremiumIcon from '@mui/icons-material/WorkspacePremium';
 import ErrorIcon from '@mui/icons-material/Error';
+
+/**
+ * 
+ * @param {*} title 
+ * @param {*} body 
+ * @param {*} type 
+ * @returns 
+ * 
+ * Componet for title card 
+ */
 function TitleCard(title, body, type) {
-    const navigate = useNavigate();
+	const navigate = useNavigate();
 
-    return (
-        <button className={styles.dashboardCard} onClick={() => { navigate('') }}>
-            {/* <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEdP1P4S9jEQDRT3Ccix1TLyVYdYfmjAUFBQ&s" alt="" className={styles.dashboardImage} /> */}
-            {type === 'active' && <ErrorIcon color="primary" fontSize="large"/>}
-            {type === 'complete' && <CheckCircleIcon color="success" fontSize="large"/>}
-            {type === 'pending' && <AccessTimeFilledIcon color="disabled" fontSize="large"/>}
-            {type === 'cert' && <WorkspacePremiumIcon sx={{ color: 'orange' }} fontSize="large"/>}
+	return (
+		<button className={styles.dashboardCard} onClick={() => { navigate('') }}>
+			{type === 'active' && <ErrorIcon color="primary" fontSize="large"/>}
+			{type === 'complete' && <CheckCircleIcon color="success" fontSize="large"/>}
+			{type === 'pending' && <AccessTimeFilledIcon color="disabled" fontSize="large"/>}
+			{type === 'cert' && <WorkspacePremiumIcon sx={{ color: 'orange' }} fontSize="large"/>}
 
-            <h1 className={styles.dashboardCardTitle}>{title}</h1>
-            <div className={styles.dashboardCardBody}>{body}</div>
-        </button>
-    )
+			<h1 className={styles.dashboardCardTitle}>{title}</h1>
+			<div className={styles.dashboardCardBody}>{body}</div>
+		</button>
+	)
 }
 
 export default TitleCard;
