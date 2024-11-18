ce10803817934a7222c251a1595589327cf6d3c8
Ce Min Pangastur <cm.pang25@gmail.com>
2024-11-19 01:36:13 +1100

    style

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,35 +4,38 @@ import SearchIcon from '@mui/icons-material/Search';
 import { useNavigate } from 'react-router-dom';
 import image from '../assets/projector2.png';
 
-
+/**
+ * Style component for image
+ */
 const imgStyle = {
   position:'absolute',
-  // top: '10',
   width: '20%',
-  // bottom: '',
   top: '53%',
-  // left: '10%',
 };
+
+/**
+ * Home page for Projector platform.
+ * 
+ * @returns 
+ */
 function Home() {
   const navigate = useNavigate();
   return (
     <>
-    <div className={styles.titleContainer}>
-    <img src={image} style={imgStyle} />
-      <h1 className={styles.title}>Find Your Dream Project Today!</h1>
-      {/* <h2 className={styles.title}>Find Your Dream Project Today!</h2> */}
-      <div class={styles.searchBar}>
-        <input 
-          type="text" 
-          className={styles.search}
-          placeholder="Search for a Project Title or Company" 
-        />
-        <button className={styles.searchButton} onClick={()=>navigate('/allprojects')}>
-          <SearchIcon fontSize="medium"></SearchIcon>
-        </button>
-        
+      <div className={styles.titleContainer}>
+      <img src={image} style={imgStyle} />
+        <h1 className={styles.title}>Find Your Dream Project Today!</h1>
+        <div class={styles.searchBar}>
+          <input 
+            type="text" 
+            className={styles.search}
+            placeholder="Search for a Project Title or Company" 
+          />
+          <button className={styles.searchButton} onClick={()=>navigate('/allprojects')}>
+            <SearchIcon fontSize="medium"></SearchIcon>
+          </button>
+        </div>
       </div>
-    </div>
     </>
   );
 }
